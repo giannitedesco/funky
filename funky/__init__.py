62bf9917cb76937ac5bd290c584bb72b63223c37
Gianni Tedesco <gianni@scaramanga.co.uk>
2016-08-08 01:53:58 +0900

    Move SeverWindow in to separate module
    It's pretty much done with

--- funky/__init__.py
+++ funky/__init__.py
@@ -1,4 +1,5 @@
 from message import Message
+from serverwindow import ServerWindow
 import client, server
 
 def xorp(k, c):
@@ -8,10 +9,28 @@ def xorp(k, c):
 	p = ''.join(chr(a ^ b) for (a,b) in izip(c, cycle(k)))
 	return p
 
+def encrypt_login(x):
+	key = '$Revision: 1.183 $'
+	x = client.LoginClientMsg(
+		enctyp = 'XORP',
+		enckey = key,
+		nam = xorp(key, x.nam),
+		pwd = xorp(key, x.pwd),
+		client_type = xorp(key, x.client_type),
+		client_id = xorp(key, x.client_id),
+		jver = xorp(key, x.jver),
+		osver = xorp(key, x.osver),
+		client_ver = xorp(key, x.client_ver),
+		mac = xorp(key, x.mac),
+	)
+
+	return x
 
 __all__ = (
 		'client',
+		'encrypt_login',
 		'server',
 		'xorp',
 		'Message',
+		'ServerWindow',
 	)
---
+++ funky/serverwindow.py
@@ -0,0 +1,91 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, GObject, Pango
+
+class CmdEntry(Gtk.Entry):
+	__gsignals__ = {
+		'send': (GObject.SIGNAL_RUN_LAST, None, (str, )),
+	}
+	def send(self, s):
+		self.emit('send', s)
+
+	def __activate(self, _):
+		s = self.get_text()
+		self.set_text('')
+		if len(s):
+			self.send(s)
+
+	def __init__(self):
+		super(CmdEntry, self).__init__()
+		self.connect('activate', self.__activate)
+
+class ServerWindow(Gtk.Box):
+	__gsignals__ = {
+		'server_cmd': (GObject.SIGNAL_RUN_LAST, None, (str, )),
+	}
+	def __setup_tags(self, buf):
+		tag = buf.create_tag('font')
+		tag.set_property('font', 'Lucida Console 8')
+
+		tag = buf.create_tag('bold')
+		tag.set_property('weight', Pango.Weight.BOLD)
+
+		for x in ['red', 'blue', 'green',
+				'cyan', 'magenta', 'yellow',
+				'purple', 'black',
+				'dark blue', 'dark green']:
+			tag = buf.create_tag(x)
+			tag.set_property('foreground', x)
+			tag.set_property('foreground-set', True)
+
+	def __cb(self, _, s):
+		self.emit('server_cmd', s)
+
+	def __init__(self):
+		super(ServerWindow, self).__init__(\
+				orientation = Gtk.Orientation.VERTICAL,
+				spacing = 6)
+
+		self.entry = CmdEntry()
+		self.entry.connect('send', self.__cb)
+
+		self.text = Gtk.TextView()
+		self.text.set_editable(False)
+		self.text.set_cursor_visible(False)
+		self.text.set_wrap_mode(Gtk.WrapMode.WORD)
+		self.__setup_tags(self.text.get_buffer())
+		scr = Gtk.ScrolledWindow()
+		scr.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+		scr.add(self.text)
+
+		scr.set_can_focus(False)
+		self.text.set_can_focus(False)
+
+		self.pack_start(scr, True, True, 0)
+		self.pack_start(self.entry, False, True, 0)
+
+	def msg(self, msg, tags = []):
+		tags.append('font')
+		buf = self.text.get_buffer()
+		i = buf.get_iter_at_offset(buf.get_char_count())
+		buf.place_cursor(i)
+		buf.insert_with_tags_by_name(i, msg, *tags)
+		i = buf.get_iter_at_offset(buf.get_char_count())
+		buf.place_cursor(i)
+
+		if not '\n' in msg:
+			return
+
+		mark = buf.create_mark(None, i, left_gravity = True)
+		self.text.scroll_to_mark(mark, 0.0, False, 0.0, 0.0)
+
+	def on_rx_cmd(self, _, msg):
+		self.msg('<<< ' + msg.cmd + '\n', ['bold'])
+
+	def on_rx(self, _, msg):
+		self.msg('<<< ' + str(msg) + '\n', ['dark green'])
+
+	def on_tx(self, _, msg):
+		self.msg('>>> ' + str(msg) + '\n', ['purple'])
+		return
+
